554d89664cae04e4db6ea056e2a622e460ad3a57
Siddhesh Ravindra More <94625517+Siddesh0002T@users.noreply.github.com>
2024-07-11 15:24:52 +0530

    get Stated page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,36 @@ import './App.css';
 function App() {
   return (
     <div class="container">
-      <h1>Hello ! </h1>
-      <h3>Welcome To <b> Rubby Room Chat </b></h3>
-    </div>
+      <center>
+          <h1 >R</h1>
+          <h1>U</h1>
+          <h1>B</h1>
+          <h1>B</h1>
+          <h1>Y</h1>
+        </center>
+      <h1 className='wel'>Hello ! </h1>
+      <h3>Welcome To 
+        <b> Rubby Room Chat </b>
+      </h3>
+      <center>
+      <div class="card">
+          <p class="heading">
+              Rubby Room Chat
+          </p>
+          <p>
+              Powered By
+          </p>
+          <p>RubbySoft
+          </p>
+          </div>
+          
+          <button>Get Started</button>
+          <h1>S</h1>
+          <h1>O</h1>
+          <h1>F</h1>
+          <h1>T</h1>
+          </center>
+  </div>    
   );
 }
 
